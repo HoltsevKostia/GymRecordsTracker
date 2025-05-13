cd6a1058ca7cbe93bfdc737f5a08e696395d5e3d
Костянтин Гольцев <ipz224_gko@student.ztu.edu.ua>
2025-05-13 20:29:07 +0300

    Favorite Service and Repo fix

---
+++ GymRecordsTracker.Server/Controllers/FavoriteContorller.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyVibe.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FavoriteContorller : ControllerBase
+    {
+
+    }
+}
--- GymRecordsTracker.Server/Services/Favorite/FavoriteService.cs
+++ GymRecordsTracker.Server/Services/Favorite/FavoriteService.cs
@@ -1,6 +1,51 @@
-﻿namespace MyVibe.Server.Services.Favorite
+﻿using AutoMapper;
+using MyVibe.Server.Models.DTO.Favorite;
+using MyVibe.Server.Repositories.Favorite;
+
+namespace MyVibe.Server.Services.Favorite
 {
-    public class FavoriteService
+    public class FavoriteService : IFavoriteService
     {
+        private readonly IFavoriteRepository _favoriteRepository;
+        private readonly IMapper _mapper;
+
+        public FavoriteService(IFavoriteRepository favoriteRepository, IMapper mapper)
+        {
+            _favoriteRepository = favoriteRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<FavoriteDTO>> GetFavoritesByUserIdAsync(int userId)
+        {
+            var favorites = await _favoriteRepository.GetAllByUserIdAsync(userId);
+            var result = favorites.Select(f => _mapper.Map<FavoriteDTO>(f));
+            return result;
+        }
+
+        public async Task<FavoriteDTO?> GetFavoriteByIdAsync(int id, int userId)
+        {
+            var favorite = await _favoriteRepository.GetByIdAndUserIdAsync(id, userId);
+            var result = favorite == null ? null : _mapper.Map<FavoriteDTO>(favorite);
+            return result;
+        }
+
+        public async Task<bool> AddFavoriteAsync(AddFavoriteDTO favoriteDTO)
+        {
+            var favorite = _mapper.Map<Models.Domain.Favorite>(favoriteDTO);
+            await _favoriteRepository.AddAsync(favorite);
+            return true;
+        }
+
+        public async Task<bool> DeleteFavoriteAsync(int id, int userId)
+        {
+            var favorite = await _favoriteRepository.GetByIdAndUserIdAsync(id, userId);
+            if (favorite == null)
+            {
+                return false;
+            }
+
+            await _favoriteRepository.DeleteAsync(favorite);
+            return true;
+        }
     }
 }
