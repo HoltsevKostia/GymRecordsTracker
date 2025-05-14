c661b2f1877c2bedcd4021558cf9964634653db8
Костянтин Гольцев <ipz224_gko@student.ztu.edu.ua>
2025-05-14 22:55:01 +0300

    Add cookie popup

--- gymrecordstracker.client/src/App.tsx
+++ gymrecordstracker.client/src/App.tsx
@@ -7,6 +7,7 @@ import HomePage from './pages/HomePage';
 import { useUserStore } from './stores/userStore';
 import Layout from './pages/Layout';
 import NotFound from './pages/NotFound';
+import CookieConsent from "react-cookie-consent";
 
 function PrivateRoute({ element }: { element: JSX.Element }) {
     const { user, loading } = useUserStore();
@@ -48,8 +49,24 @@ function App() {
             ]
         }
     ]);
+    return (
+        <>
+            <RouterProvider router={router} />;
 
-    return <RouterProvider router={router} />;
+            <CookieConsent
+                location="bottom"
+                cookieName="my_vibe_consent"
+                style={{ background: "#2B373B" }}
+                buttonStyle={{ color: "#fff", backgroundColor: "#1976d2", fontSize: "14px" }}
+                buttonText="Accept"
+                enableDeclineButton={false}
+                contentStyle={{ marginRight: "12px" }}
+            >
+                We only use technical cookies necessary for login and session.
+                <a href="/privacy-policy" style={{ color: "#90caf9", marginLeft: 8 }}>More info</a>
+            </CookieConsent>
+        </>
+    );
 }
 
 export default App;
