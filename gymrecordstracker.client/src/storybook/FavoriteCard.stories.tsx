4204f237a8a39e42ce464787df875dbf1ba21637
Костянтин Гольцев <ipz224_gko@student.ztu.edu.ua>
2025-05-15 19:34:32 +0300

    Add storybook. Describe 2 components

---
+++ gymrecordstracker.client/src/storybook/FavoriteCard.stories.tsx
@@ -0,0 +1,45 @@
+import { Meta, StoryFn } from "@storybook/react";
+import { FavoriteCard } from "../components/ui-ux/favoriteCard";
+import { FavoriteDTO } from "../interfaces/favoriteInterfaces";
+import { Box } from "@mui/material";
+
+export default {
+    title: "Components/FavoriteCard",
+    component: FavoriteCard,
+} as Meta;
+
+const Template: StoryFn<{ favorite: FavoriteDTO }> = (args) => (
+    <Box width={300}>
+        <FavoriteCard {...args} />
+    </Box>
+);
+
+export const MemeCard = Template.bind({});
+MemeCard.args = {
+    favorite: {
+        id: 1,
+        userId: 1,
+        type: 1,
+        content: "https://glavcom.ua/img/article/8684/6_main-v1660641284.jpg",
+    },
+};
+
+export const ImageCard = Template.bind({});
+ImageCard.args = {
+    favorite: {
+        id: 2,
+        userId: 1,
+        type: 0,
+        content: "https://picsum.photos/300",
+    },
+};
+
+export const QuoteCard = Template.bind({});
+QuoteCard.args = {
+    favorite: {
+        id: 3,
+        userId: 1,
+        type: 2,
+        content: "Do or do not. There is no try.",
+    },
+};
---
+++ gymrecordstracker.client/src/storybook/FormInput.stories.tsx
@@ -0,0 +1,43 @@
+import { useForm } from "react-hook-form";
+import { Meta, StoryFn } from "@storybook/react";
+import FormInput from "../components/ui-ux/formInput";
+
+export default {
+    title: "Components/FormInput",
+    component: FormInput,
+} as Meta;
+
+const Template: StoryFn<any> = (args) => {
+    const { control, formState: { errors } } = useForm({
+        defaultValues: {
+            [args.name]: "",
+        },
+    });
+
+    return (
+        <form>
+            <FormInput {...args} control={control} errors={errors} />
+        </form>
+    );
+};
+
+export const TextInput = Template.bind({});
+TextInput.args = {
+    name: "username",
+    label: "Username",
+    type: "text",
+};
+
+export const EmailInput = Template.bind({});
+EmailInput.args = {
+    name: "email",
+    label: "Email",
+    type: "email",
+};
+
+export const PasswordInput = Template.bind({});
+PasswordInput.args = {
+    name: "password",
+    label: "Password",
+    type: "password",
+};
